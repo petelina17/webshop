d5adfd54c6d2fa6e8b0234e94e2a6380fa27d3fb
petelina17 <petelina17@gmail.com>
2020-04-01 21:58:55 +0200

    product details complete + add to cart button

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,12 +1,58 @@
-import React from "react"
-import {useParams} from 'react-router-dom'
+import React from 'react'
+import {Link, useParams} from 'react-router-dom'
+import {ProductData} from './ProductWidget'
+import {Button, Card, CardActions, CardContent, Container, Typography} from '@material-ui/core'
+import {addProductData} from '../store'
+import {on} from 'cluster'
 
 export default function ProductDetail() {
 
   let {id} = useParams()
 
-  return(
+  // temporary solution
+  const allProducts = require('../assets/product-data/product-data.json')
+  const product: ProductData = allProducts.find((p: ProductData) => p.id.toString() === id)
 
-      <h1>DETAILS for {id}</h1>
+  const onCartButtonClick = () => {
+    // this.props.onCartIconClick(this.props.productData)
+    addProductData(product)
+    // console.log(appStore.cartList)
+  }
+
+  return (
+      <Container maxWidth="md">
+        <Card>
+          <CardContent>
+            <Typography variant="h3" gutterBottom>
+              {product.name}
+            </Typography>
+
+            <div style={{textAlign: 'center', padding: '3rem'}}>
+              <img style={{width: '100%', maxWidth: '700px'}} src={product.image} alt={product.name}/>
+            </div>
+
+            <Typography variant="h5" gutterBottom>
+              <strong>Produkt detaljer: </strong>
+              <div>{product.description}</div>
+            </Typography>
+
+            <Typography variant="h5" gutterBottom>
+              <strong>Pris: <span style={{color: 'red'}}>{product.salePrice.toFixed(2) + ' kr'}</span></strong>
+            </Typography>
+
+            <Typography variant="h5" gutterBottom>
+              <strong>Packning: </strong>{product.measure}
+            </Typography>
+
+          <CardActions>
+            <Button variant="contained" color="secondary" size="large" onClick={onCartButtonClick}>
+              <Typography variant="h5">
+                Lägg i Kundvagnen
+              </Typography>
+            </Button>
+          </CardActions>
+          </CardContent>
+        </Card>
+      </Container>
   )
 }
