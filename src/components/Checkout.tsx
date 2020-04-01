42da7b61d8620edc8d2465d4584ede6ff9a9074b
petelina17 <petelina17@gmail.com>
2020-04-01 23:58:25 +0200

    WIP: checkout flow

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
-import {Button, Container, Typography} from '@material-ui/core'
+import {Button, Card, CardContent, Container, createStyles, Theme, Typography} from '@material-ui/core'
 import Cart from './Cart'
+import {makeStyles} from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      root: {
+        marginTop: '1rem',
+        marginBottom: '2rem',
+      },
+    }),
+);
+
 
 export default function Checkout() {
+  const classes = useStyles();
 
   const onFinish = () => {}
 
@@ -13,6 +25,46 @@ export default function Checkout() {
         </Typography>
 
         <Cart pagination={false} />
+
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography variant="h5" gutterBottom>
+              {'Dina uppgifter'.toUpperCase()}
+            </Typography>
+
+            <p>123</p>
+            <p>123</p>
+            <p>123</p>
+
+          </CardContent>
+        </Card>
+
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography variant="h5" gutterBottom>
+              LEVERANS
+            </Typography>
+
+            <p>123</p>
+            <p>123</p>
+            <p>123</p>
+
+          </CardContent>
+        </Card>
+
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography variant="h5" gutterBottom>
+              BETALNINGSSÄTT
+            </Typography>
+
+            <p>123</p>
+            <p>123</p>
+            <p>123</p>
+
+          </CardContent>
+        </Card>
+
 
 
         <Button variant="contained" color="secondary" size="large" onClick={onFinish}>
