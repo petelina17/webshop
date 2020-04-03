3dabee83cdd6aac637012c5aa81a830d79746470
petelina17 <petelina17@gmail.com>
2020-04-03 14:39:54 +0200

    Updates

---
+++ precache-manifest.51189dad72e0f0b2d74b80ce8ba21adc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee638feef79b5ebcfaa2528e54560f91",
+    "url": "/webshop/index.html"
+  },
+  {
+    "revision": "c098f3553138dbe92fc5",
+    "url": "/webshop/static/css/main.5991154e.chunk.css"
+  },
+  {
+    "revision": "fc70dc50f122f91cfa87",
+    "url": "/webshop/static/js/2.0690da04.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/webshop/static/js/2.0690da04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c098f3553138dbe92fc5",
+    "url": "/webshop/static/js/main.5c4ac27f.chunk.js"
+  },
+  {
+    "revision": "92285c910c408aadff6c",
+    "url": "/webshop/static/js/runtime-main.0763743a.js"
+  }
+]);
